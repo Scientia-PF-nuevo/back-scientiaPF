2ffdfeec3dab8b78ccdf47ef1188acc15c2771fd
jonathanvolker <joni_451@hotmail.com>
2021-10-22 11:16:45 -0300

    Agregado stock Marcos

--- src/routes/Purchase.js
+++ src/routes/Purchase.js
@@ -1,7 +1,6 @@
 const server = require('express').Router()
-const { Course, Category, Review, User, Bought_course, Order } = require('../db');
+const { Course, Category, Review, User, Bought_course, Order, Stock } = require('../db');
 const axios = require('axios');
-const { response } = require('express');
 const mercadopago = require('mercadopago');
 // const token = 'TEST-5014021276587978-102020-2aa0263c739b5941b77085e513aa6fad-90743208';
 mercadopago.configure({
@@ -11,61 +10,147 @@ mercadopago.configure({
 server.post('/:email', async (req, res) => {
     const email = req.params.email;
     const {token,payment_method_id,issuer_id,installments,payer}=req.body
-    const user = await User.findOne({
-        where: {
-            email: email
-        }
-    })
-    let totalPrice = 0;
-    const coursesIds = [];
-    const fetching = axios.get(`http://localhost:3001/order/${email}`)
-        .then(async (curses) => {
-            const data = curses.data;
-            let response = data.map(async (c) => {
-                const course = await Course.findOne({
-                    where: {
-                        id: c.coursesId
-                    },
-                    attributes: ['price', 'id']
-                })
-                const price = await course.get('price')
-                const id = await course.get('id')
-               
-                totalPrice = totalPrice + price;
-                return (
-                    totalPrice,
-                    coursesIds.push(id))
-            })
-            Promise.all(response).then(async() => {
-                try {
-                    const payment_data = {
-                    transaction_amount:totalPrice,
-                    token, 
-                    description: 'payment',
-                    installments,
-                    payment_method_id,
-                    issuer_id,
-                    payer
-                }
-                mercadopago.payment
-                .save(payment_data)
-                .then((r)=>{
-                    if(r.status===201){
-                        const destroy= axios.post(`http://localhost:3001/purchase/orders_destroy/${email}`)
-                    }
-                    return res.status(r.status).json({
-                        status:r.body.status,
-                        status_detail:r.body.detail,
-                        id:r.body.id
-                    })
-                })}catch(e){
-                    return res.status(500).send(e)
-                }
-                
-
-           
-            })
+    const disc = req.body.disc ? req.body.disc : 0; 
+    
+    if (disc === 0) {
+        const user = await User.findOne({
+            where: {
+                email: email
+            }
         })
+        let totalPrice = 0;
+        const coursesIds = [];
+        const fetching = axios.get(`http://localhost:3001/order/${email}`)
+            .then(async (curses) => {
+                const data = curses.data;
+                let response = data.map(async (c) => {
+                    const course = await Course.findOne({
+                        where: {
+                            id: c.coursesId
+                        },
+                        attributes: ['price', 'id']
+                    })
+                    const price = await course.get('price')
+                    const id = await course.get('id')
+                   
+                    totalPrice = totalPrice + price;
+                    return (
+                        totalPrice,
+                        coursesIds.push(id))
+                })
+                Promise.all(response).then(async() => {
+                    try {
+                        const payment_data = {
+                        transaction_amount:totalPrice,
+                        token, 
+                        description: 'payment',
+                        installments,
+                        payment_method_id,
+                        issuer_id,
+                        payer
+                    }
+                    mercadopago.payment
+                    .save(payment_data)
+                    .then((r)=>{
+                        if(r.status===201){
+                            const destroy= axios.post(`http://localhost:3001/purchase/orders_destroy/${email}`)
+                        }
+                        return res.status(r.status).json({
+                            status:r.body.status,
+                            status_detail:r.body.detail,
+                            id:r.body.id
+                        })
+                    })}catch(e){
+                        return res.status(500).send(e)
+                    }
+                    
+    
+               
+                })
+            })
+    }else{
+        const descuento = await Stock.findOne({
+            where:{discountId: disc}
+        })
+        if(descuento === null){
+            return res.json({msg:"el codigo no existe o ya fue utilizado demaciadas veces"})
+        }
+        if(descuento !== null){
+            const ifActive= descuento.ifActive
+            const amount = descuento.amount
+            const percentage = descuento.percentage
+            if(!ifActive || amount === 0){
+                return res.json({msg:"el codigo de descuento fue utilizado demaciadas veces"})
+            }
+            const user = await User.findOne({
+                where: {
+                    email: email
+                }
+            })
+            let totalPrice = 0;
+            const coursesIds = [];
+            const fetching = axios.get(`http://localhost:3001/order/${email}`)
+                .then(async (curses) => {
+                    const data = curses.data;
+                    let response = data.map(async (c) => {
+                        const course = await Course.findOne({
+                            where: {
+                                id: c.coursesId
+                            },
+                            attributes: ['price', 'id']
+                        })
+                        const price = await course.get('price')
+                        const id = await course.get('id')
+                       
+                        totalPrice = totalPrice + price;
+                        return (
+                            totalPrice,
+                            coursesIds.push(id))
+                    })
+                    Promise.all(response).then(async() => {
+                        try {
+                            const finalPrice = totalPrice * percentage
+                            const payment_data = {
+                            transaction_amount:finalPrice,
+                            token, 
+                            description: 'payment',
+                            installments,
+                            payment_method_id,
+                            issuer_id,
+                            payer
+                        }
+                        mercadopago.payment
+                        .save(payment_data)
+                        .then((r)=>{
+                            if(r.status===201){
+                                const destroy= axios.post(`http://localhost:3001/purchase/orders_destroy/${email}`)
+                            }
+                            const updated = await Stock.update({
+                                amount: -- amount,
+                                active: --amount === 0 ? false : true
+                            }, {where : {
+                                discountId : disc
+                            }}
+                            )
+
+                            return res.status(r.status).json({
+                                status:r.body.status,
+                                status_detail:r.body.detail,
+                                id:r.body.id
+                            })
+                        })}catch(e){
+                            return res.status(500).send(e)
+                        }
+                        
+        
+                   
+                    })
+                })
+
+
+        }
+    }
+   
 
 })
 
