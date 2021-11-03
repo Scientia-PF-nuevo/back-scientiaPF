e2c91f5917a6da47dcb9689f5e4c6733ce04ebe6
jonathanvolker <joni_451@hotmail.com>
2021-11-03 12:14:44 -0300

    correccion de link

--- src/routes/Order.js
+++ src/routes/Order.js
@@ -14,7 +14,7 @@ var local = "";
 if(process.env.PORT == 3001){
   local = "http://localhost:3001";
 }else{
-  local = "https://scientiapfdeploy.herokuapp.com/";
+  local = "https://scientiapfdeploy.herokuapp.com";
 }
 
 
